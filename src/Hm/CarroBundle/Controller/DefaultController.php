0e6f5d870986897bb492bcae8788c144902ace78
hairton <hairtonsena@yahoo.com.br>
2016-10-04 14:11:02 -0300

    Conclusao crud carro

--- src/Hm/CarroBundle/Controller/DefaultController.php
+++ src/Hm/CarroBundle/Controller/DefaultController.php
@@ -7,72 +7,43 @@ use Hm\CarroBundle\Entity\Carro;
 use Hm\CarroBundle\Form\CarroType;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
 
-    public function indexAction()
-    {
+    public function indexAction() {
 
         $em = $this->getDoctrine()->getManager();
 
         $carros = $em->getRepository("HmCarroBundle:Carro")->findAll();
 
-
         return $this->render('HmCarroBundle:Default:index.html.twig', array("carros" => $carros)
         );
     }
 
-
     public function addAction(Request $request) {
-
-
         $carro = new Carro();
         $form = $this->createCreateForm($carro);
 
         $form->handleRequest($request);
 
-        if($form->isSubmitted()){
-        
-        $carro = $form->getData();
+        if ($form->isSubmitted()) {
 
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($carro);
-        $em->flush();
-        return $this->redirectToRoute('hm_carro');
-        
+            $carro = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($carro);
+            $em->flush();
+            return $this->redirectToRoute('hm_carro');
         }
-        
-        
+
         return $this->render('HmCarroBundle:Default:form_add.html.twig', array('form' => $form->createView()));
     }
 
-
-    private function createCreateForm(Carro $carro)
-    {
-        $form = $this->createForm(CarroType::class, $carro, array('action' => $this->generateUrl('hm_carro_salvar'), 'method' => "POST"));
+    private function createCreateForm(Carro $carro) {
+        $form = $this->createForm(CarroType::class, $carro, array('action' => $this->generateUrl('hm_carro_add'), 'method' => "POST"));
         return $form;
     }
 
-    public function salvarAction(Request $request)
-    {
-
-        $carro = new Carro();
-
-        $form = $this->createCreateForm($carro);
-
-        $form->handleRequest($request);
-
-        $carro = $form->getData();
-
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($carro);
-        $em->flush();
-        return $this->redirectToRoute('hm_carro');
-    }
-
-
     public function editarAction(Request $request, $id) {
-
 
         $em = $this->getDoctrine()->getManager();
         $carro = $em->getRepository("HmCarroBundle:Carro")->find($id);
@@ -94,8 +65,7 @@ class DefaultController extends Controller
         return $this->render('HmCarroBundle:Default:form_edit.html.twig', array('form' => $form->createView()));
     }
 
-    private function createFormEdit(Carro $carro)
-    {
+    private function createFormEdit(Carro $carro) {
         $form = $this->createForm(CarroType::class, $carro, array(
             'action' => $this->generateUrl('hm_carro_editar', array('id' => $carro->getId())),
             'method' => 'PUT',
@@ -104,31 +74,7 @@ class DefaultController extends Controller
         return $form;
     }
 
-
-    public function salvarUpAction(Request $request, $id)
-    {
-
-        $em = $this->getDoctrine()->getManager();
-        $carro = $em->getRepository('HmCarroBundle:Carro')->find($id);
-
-        // $carro = new Carro();
-
-
-        $form = $this->createFormEdit($carro);
-
-        $form->handleRequest($request);
-
-        // $carro = $form->getData();
-
-
-        // $em->persist($carro);
-        $em->flush();
-
-        return $this->redirectToRoute('homepage');
-    }
-
-    public function deleteAction($id)
-    {
+    public function deleteAction($id) {
         $em = $this->getDoctrine()->getManager();
         $carro = $em->getRepository('HmCarroBundle:Carro')->find($id);
 
